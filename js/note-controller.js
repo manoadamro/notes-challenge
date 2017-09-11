466e89a6db7d152e85a119697aef32f2f1148498
Frederick Ofori <FrederickCodes@gmail.com>
2017-09-11 16:43:34 +0100

    added script to html that changes text

---
+++ js/note-controller.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+  function changeText() {
+    // document.body.innerHTML = "howdy"
+    console.log(document.getElementById('app'))
+    document.getElementById('app').innerHTML = "howdy";
+  });
+
+
+});
