4269a63be1af3c5c11247a826b6e13246e0544ee
Adam Romano <manoadamro.imp@gmail.com>
2017-09-11 12:57:24 +0100

    added note list + test

---
+++ js/note-list-model.js
@@ -0,0 +1,24 @@
+
+
+// module for 'Note'
+(function(exports) {
+
+  // constructor, takes class type of note
+  var NoteList = function () {
+    this.notes = [];
+  };
+
+  // creates a new note and adds it to array
+  NoteList.prototype.addNote = function (text) {
+    var newNote = new Note(text)
+    this.notes.push(newNote);
+  };
+
+  NoteList.prototype.getNotes = function () {
+    return this.notes;
+  }
+
+  // allows 'NoteList' to be used globally
+  exports.NoteList = NoteList;
+
+})(this);
---
+++ tests/note-list-model-tests.js
@@ -0,0 +1,18 @@
+
+
+(function(exports) {
+  function testThatNoteIsCreated() {
+
+    var text = "Hello Adam!";
+    var list = new NoteList();
+    list.addNote(text);
+
+    if (list.getNotes().length !== 1) {
+      throw new Error("New note was not added to array");
+    } else {
+      console.log("testThatNoteIsCreated:  Passes");
+    }
+  };
+
+  testThatNoteIsCreated();
+})(this);
